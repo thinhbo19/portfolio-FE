fe4791042c4c0a52f9a60536f6795c19cbb51219
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2023-12-28 01:04:33 +0700

    files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import Resume from "./components/Resume/Resume";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Pricing from "./components/Pricing/Pricing";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Contact from "./components/Contact/Contact";
+import Blog from "./components/Blog/Blog";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Sidebar />
+      <main className="main">
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Pricing />
+        <Testimonials />
+        <Blog />
+        <Contact />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Resume.csss";
+const Resume = () => {
+  return <div>Resume</div>;
+};
+
+export default Resume;

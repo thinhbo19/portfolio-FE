08dc1a42a0425e0b6d55b147f9ca75beae3158ec
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-02-08 18:42:59 +0700

    testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import "./Testimonials.css";
+import Data from "./Data";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
   return (
     <section className="testimonials container section" id="Testimonials">
-      <h2 className="section_title">Testimonials</h2>
+      <h2 className="section_title">Clients & Reviews</h2>
+      <Swiper
+        className="testimonials__container grid"
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        grabCursor={true}
+        pagination={{ clickable: true }}
+      >
+        {Data.map(({ id, image, title, subtitle, comment }) => {
+          return (
+            <SwiperSlide className="testimonial__item" key={id}>
+              <div className="thumb">
+                <img src={image} alt="" />
+              </div>
+              <h3 className="testimonials__title">{title}</h3>
+              <span className="subtitle">{subtitle}</span>
+              <div className="comment">{comment}</div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
